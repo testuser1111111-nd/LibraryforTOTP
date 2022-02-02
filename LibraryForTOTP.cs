96c4c490174c83bb5f73c3299a00b6a0513cb192
I T <tbsa334zn6@outlook.jp>
2022-02-02 17:50:41 +0900

    idk why i wrote this

--- LibraryForTOTP.cs
+++ LibraryForTOTP.cs
@@ -37,68 +37,7 @@ namespace LibraryForTOTP
     {
         public static int CharToInt(char c)
         {
-            switch (c)
-            {
-                case 'a': return 0;
-                case 'b': return 1;
-                case 'c': return 2;
-                case 'd': return 3;
-                case 'e': return 4;
-                case 'f': return 5;
-                case 'g': return 6;
-                case 'h': return 7;
-                case 'i': return 8;
-                case 'j': return 9;
-                case 'k': return 10;
-                case 'l': return 11;
-                case 'm': return 12;
-                case 'n': return 13;
-                case 'o': return 14;
-                case 'p': return 15;
-                case 'q': return 16;
-                case 'r': return 17;
-                case 's': return 18;
-                case 't': return 19;
-                case 'u': return 20;
-                case 'v': return 21;
-                case 'w': return 22;
-                case 'x': return 23;
-                case 'y': return 24;
-                case 'z': return 25;
-                case 'A': return 0;
-                case 'B': return 1;
-                case 'C': return 2;
-                case 'D': return 3;
-                case 'E': return 4;
-                case 'F': return 5;
-                case 'G': return 6;
-                case 'H': return 7;
-                case 'I': return 8;
-                case 'J': return 9;
-                case 'K': return 10;
-                case 'L': return 11;
-                case 'M': return 12;
-                case 'N': return 13;
-                case 'O': return 14;
-                case 'P': return 15;
-                case 'Q': return 16;
-                case 'R': return 17;
-                case 'S': return 18;
-                case 'T': return 19;
-                case 'U': return 20;
-                case 'V': return 21;
-                case 'W': return 22;
-                case 'X': return 23;
-                case 'Y': return 24;
-                case 'Z': return 25;
-                case '2': return 26;
-                case '3': return 27;
-                case '4': return 28;
-                case '5': return 29;
-                case '6': return 30;
-                case '7': return 31;
-                default: return -1;
-            }
+            return Array.IndexOf(table,Char.ToUpper(c));
         }
         public static byte[] FromBase32String(string base32text, char padding = '=')
         {
